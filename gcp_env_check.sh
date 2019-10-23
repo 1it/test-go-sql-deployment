8939b569b925ee9dd1a0ac32eca8a2a24bdf9857
Ivan Tuzhilkin <i.tuzhilkin@kefirgames.com>
2019-10-23 10:52:08 +0400

    Added some messages, and exit condition

--- gcp_env_check.sh
+++ gcp_env_check.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash 
 
-if ! which -s gcloud; then echo "gcloud is not installed" && exit 1; fi
-if ! which -s kubectl; then echo "kubectl is not installed" && exit 1; fi
+if ! which -s gcloud; then echo "Error: gcloud is not installed" && exit 1; fi
+if ! which -s kubectl; then echo "Error: kubectl is not installed" && exit 1; fi
 
-# OS=$(uname -s)
 GCP_PROJECT=$(gcloud config get-value core/project)
+if [ "$GCP_PROJECT" != "" ]; then
+    export GCP_PROJECT=$GCP_PROJECT; echo "Your default GCP project is $GCP_PROJECT";
+else 
+    echo "Error: GCP_PROJECT is not set.";
+    echo "Please follow the instructions and run 'gcloud init' first" && exit 1;
+fi
+
 GCP_REGION=$(gcloud config get-value compute/region)
+if [ "$GCP_REGION" != "" ]; then
+    export GCP_REGION=$GCP_REGION; echo "Your default GCP region is $GCP_REGION";
+else 
+    echo "Error: GCP_REGION is not set.";
+    echo "Please choose your default GCP region and run 'gcloud config set compute/region REGION'" && exit 1;
+fi
+
 GCP_ZONE=$(gcloud config get-value compute/zone)
-
-if [ ! -z "$GCP_PROJECT" ]; then 
-    export GCP_PROJECT=$GCP_PROJECT; 
+if [ "$GCP_ZONE" != "" ]; then
+    export GCP_ZONE=$GCP_ZONE; echo "Your default GCP zone is $GCP_ZONE";
 else 
-    echo "GCP_PROJECT is not set. Please follow the instructions and run 'gcloud init' first"
+    echo "Error: GCP_ZONE is not set.";
+    echo "Please choose your default GCP zone and run 'gcloud config set compute/zone ZONE'" && exit 1;
 fi
-
-if [ ! -z "$GCP_REGION" ]; then 
-    export GCP_REGION=$GCP_REGION; 
-else 
-    echo "GCP_REGION is not set. Please run 'gcloud init' and choose your default region"
-fi
-
-if [ ! -z "$GCP_ZONE" ]; then 
-    export GCP_ZONE=$GCP_ZONE; 
-else 
-    echo "GCP_ZONE is not set. Please run 'gcloud init' and choose your default zone"
-fi
- 
